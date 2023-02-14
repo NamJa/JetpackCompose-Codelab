a7ab8b59beac8a462067fe5389f254d093dc791b
Jongwoo Kim <kjwoo810@gmail.com>
2023-02-14 21:54:19 +0900

    Pract1: add Happy Birthday Image

--- app/src/main/java/com/jongwoo/pract/MainActivity.kt
+++ app/src/main/java/com/jongwoo/pract/MainActivity.kt
@@ -3,13 +3,19 @@ package com.jongwoo.pract
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jongwoo.pract.ui.theme.Pract1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BirthGreetingWithImage(
+                        message = getString(R.string.happy_birthday_text),
+                        from = "- from Hou"
+                    )
                 }
             }
         }
@@ -30,14 +39,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 35.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End)
+                .padding(end = 16.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun BirthGreetingWithImage(message: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BirthdayCardPreview() {
     Pract1Theme {
-        Greeting("Android")
+        BirthGreetingWithImage(message = "Happy Birthday Kim Hou!", "- from Hou")
     }
 }
