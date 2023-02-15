f89ef183587a994ac4f8811ab613b39a398f815b
Jongwoo Kim <kjwoo810@gmail.com>
2023-02-15 23:53:19 +0900

    SOLVE: PRACT1_Compose 4 Part

--- Pract1/challenge/Compose4part/app/src/main/java/com/example/compose4part/MainActivity.kt
+++ Pract1/challenge/Compose4part/app/src/main/java/com/example/compose4part/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.compose4part
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose4part.ui.theme.Compose4partTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeFourPartScreen()
                 }
             }
         }
@@ -30,17 +42,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeFourPartScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .background(Color.Cyan)
+                .weight(1f)
+                .fillMaxHeight()
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .background(Color.Green)
+                    .fillMaxHeight()
+                    .fillMaxWidth().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Text composable", fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 16.dp))
+                Text(text = "Displays text and follows Material Design guidelines.", textAlign = TextAlign.Justify)
+            }
+            Column(modifier = Modifier
+                .weight(1f)
+                .background(Color.Yellow)
+                .fillMaxHeight()
+                .fillMaxWidth().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+                Text(text = "Image composable", fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 16.dp))
+                Text(text = "Creates a composable that lays out and draws a given Painter class object.", textAlign = TextAlign.Justify)
+            }
+        }
+        Row(
+            modifier = Modifier
+                .background(Color.Blue)
+                .weight(1f, true)
+                .fillMaxHeight()
+        ) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .background(Color.Cyan)
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+                Text(text = "Row composable", fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 16.dp))
+                Text(text = "A layout composable that places its children in a horizontal sequence.", textAlign = TextAlign.Justify)
+            }
+            Column(modifier = Modifier
+                .weight(1f)
+                .background(Color.LightGray)
+                .fillMaxHeight()
+                .fillMaxWidth().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center) {
+                Text(text = "Column composable", fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 16.dp))
+                Text(text = "A layout composable that places its children in a vertical sequence.", textAlign = TextAlign.Justify)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     Compose4partTheme {
-        Greeting("Android")
+        ComposeFourPartScreen()
     }
 }
