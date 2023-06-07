42b289c49b1d1a0da6386d261c76e2c883530035
namja <kjwoo810@gmail.com>
2023-06-07 20:10:21 +0900

    stage1 codelab2 complete

--- stage1/CodeLab1/app/src/main/java/com/example/codelab1/MainActivity.kt
+++ stage1/CodeLab1/app/src/main/java/com/example/codelab1/MainActivity.kt
@@ -3,15 +3,23 @@ package com.example.codelab1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.codelab1.ui.theme.CodeLab1Theme
 
@@ -25,7 +33,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BirthdayGreetingWithText(message = "Happy Birthday Sam", from = "- from emma")
+                    BirthdayGreetingWithImage(
+                        message = stringResource(id = R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_res)
+                    )
                 }
             }
         }
@@ -33,10 +44,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreetingWithText(message: String, from: String, modifier :Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = message, fontSize = 36.sp)
-        Text(text = from, fontSize = 24.sp)
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = "",
+            contentScale = ContentScale.Crop
+        )
+    }
+    BirthdayGreetingWithText(message = message, from = from)
+}
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(text = message, fontSize = 36.sp, modifier = Modifier.padding(top = 16.dp))
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp).align(alignment = Alignment.End)
+        )
     }
 }
 
