5b15b102f4bf22e00a1dcdcd0834f16faa28824c
namja <kjwoo810@gmail.com>
2023-08-13 21:58:06 +0900

    add Unit & UI Tests

--- stage2_2/Codelab2_2/app/src/main/java/com/example/codelab2_2/MainActivity.kt
+++ stage2_2/Codelab2_2/app/src/main/java/com/example/codelab2_2/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -160,7 +161,8 @@ fun RoundTheTipRow(
     }
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
     if(roundUp)
         tip = ceil(tip)
---
+++ stage2_2/Codelab2_2/app/src/test/java/com/example/codelab2_2/TipCalculatorTests.kt
@@ -0,0 +1,16 @@
+package com.example.codelab2_2
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipCalculatorTests {
+    @Test
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+    }
+}
