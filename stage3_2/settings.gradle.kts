bfbcce0211db59afa9d9f1ab5aa3f42758d76121
namja <kjwoo810@gmail.com>
2023-09-10 16:34:39 +0900

    add codelab3_2_woof

---
+++ stage3_2/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Codelab3_2_Woof"
+include(":app")
