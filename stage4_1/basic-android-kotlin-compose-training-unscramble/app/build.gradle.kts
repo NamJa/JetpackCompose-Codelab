c4b5f3b8c4556d0601a5b11cda8c748e849bb8dc
Namja <kjwoo810@gmail.como>
2024-02-18 20:47:04 +0900

    [stage4_1] update libraries

--- stage4_1/basic-android-kotlin-compose-training-unscramble/app/build.gradle.kts
+++ stage4_1/basic-android-kotlin-compose-training-unscramble/app/build.gradle.kts
@@ -67,13 +67,13 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
